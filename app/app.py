7e8869b5a201d1852bf8b1553a357e9332c483d2
Racoth505 <rafafl2001@gmail.com>
2026-04-20 09:59:44 -0600

    Fin

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
-    return jsonify({"message": "¡Despliegue de Rafael Flores Lopez!"})
+    return jsonify({"message": "¡Despliegue de Rafael Flores Lopez"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
